0c8437554a3b00f4ddd08be805b7eba1a55806b9
Alexander Samoilov <alexander.samoilov@gmail.com>
2015-05-15 12:20:55 +0300

    proof of concept: split apart values.

--- src/c/isl_wrapper.c
+++ src/c/isl_wrapper.c
@@ -22,10 +22,52 @@ JNIEXPORT jlong JNICALL Java_isl_ISL_valZero
     return (long)val;
 }
 
+JNIEXPORT jlong JNICALL Java_isl_ISL_valOne
+(JNIEnv *env, jobject obj, jlong lp) {
+    isl_ctx *ctx = (isl_ctx*)lp;
+    isl_val *val = isl_val_one(ctx);
+    return (long)val;
+}
+
+JNIEXPORT jlong JNICALL Java_isl_ISL_valNegOne
+(JNIEnv *env, jobject obj, jlong lp) {
+    isl_ctx *ctx = (isl_ctx*)lp;
+    isl_val *val = isl_val_negone(ctx);
+    return (long)val;
+}
+
+JNIEXPORT jlong JNICALL Java_isl_ISL_valNan
+(JNIEnv *env, jobject obj, jlong lp) {
+    isl_ctx *ctx = (isl_ctx*)lp;
+    isl_val *val = isl_val_nan(ctx);
+    return (long)val;
+}
+
+JNIEXPORT jlong JNICALL Java_isl_ISL_valInfty
+(JNIEnv *env, jobject obj, jlong lp) {
+    isl_ctx *ctx = (isl_ctx*)lp;
+    isl_val *val = isl_val_infty(ctx);
+    return (long)val;
+}
+
+JNIEXPORT jlong JNICALL Java_isl_ISL_valNegInfty
+(JNIEnv *env, jobject obj, jlong lp) {
+    isl_ctx *ctx = (isl_ctx*)lp;
+    isl_val *val = isl_val_neginfty(ctx);
+    return (long)val;
+}
+
 JNIEXPORT jboolean JNICALL Java_isl_ISL_valIsZero
 (JNIEnv *env, jobject obj, jlong lp) {
     isl_val *val = (isl_val*)lp;
     jboolean result = (jboolean)isl_val_is_zero(val);
+    return result;
+}
+
+JNIEXPORT jboolean JNICALL Java_isl_ISL_valIsOne
+(JNIEnv *env, jobject obj, jlong lp) {
+    isl_val *val = (isl_val*)lp;
+    jboolean result = (jboolean)isl_val_is_one(val);
     return result;
 }
 
--- src/main/scala/isl/ISLwrapper.scala
+++ src/main/scala/isl/ISLwrapper.scala
@@ -1,11 +1,16 @@
 package isl
 
-class ISL {
+case class Pointer(ptr: Long)
+
+//def deref(p: Pointer) = p.ptr
+
+private[isl] class ISL {
   // context
   @native def createContext: Long
   @native def releaseContext(ctx: Long): Unit
 
   // values
+    // creation
   @native def valZero(ctx: Long): Long
   @native def valOne(ctx: Long): Long
   @native def valNegOne(ctx: Long): Long
@@ -13,22 +18,58 @@ class ISL {
   @native def valInfty(ctx: Long): Long
   @native def valNegInfty(ctx: Long): Long
 
+    // queries
   @native def valIsZero(value: Long): Boolean
+  @native def valIsOne(value: Long): Boolean
 
+}
+
+// a singleton for keeping a single instance and context
+object ISL {
+  System.loadLibrary("isl_jni")
+  val isl = new ISL
+  val ctx = isl.createContext
+}
+
+case class ISL_context(ctx: Pointer)
+
+private[isl] trait ISL_value_iface {
+  
+  def zero:     ISL_value
+  def one:      ISL_value
+  def negOne:   ISL_value
+  def nan:      ISL_value
+  def infty:    ISL_value
+  def negInfty: ISL_value
+
+  def isZero (other: ISL_value): Boolean
+  def isOne  (other: ISL_value): Boolean
+
+}
+
+case class ISL_value(value: Pointer = Pointer(0L)) extends ISL_value_iface {
+  private val isl = ISL.isl
+  private val ctx = ISL.ctx
+
+  def zero     = ISL_value( Pointer(isl.valZero     (ctx)))
+  def one      = ISL_value( Pointer(isl.valOne      (ctx)))
+  def negOne   = ISL_value( Pointer(isl.valNegOne   (ctx)))
+  def nan      = ISL_value( Pointer(isl.valNan      (ctx)))
+  def infty    = ISL_value( Pointer(isl.valInfty    (ctx)))
+  def negInfty = ISL_value( Pointer(isl.valNegInfty (ctx)))
+
+  def isZero (other: ISL_value): Boolean = isl.valIsZero (other.value.ptr)
+  def isOne  (other: ISL_value): Boolean = isl.valIsOne  (other.value.ptr)
 }
 
 // --- Code in App body will get wrapped in a main method on compilation
-object ISL extends App {
-  // --- Main method to test native library
-  System.loadLibrary("isl_jni")
-  val isl = new ISL
-  private val ctx = isl.createContext
+object ISL_sample extends App {
 
   // do something useful
-  val zero = isl.valZero(ctx)
-  println("val is zero? " + isl.valIsZero(zero))
+  val isl_value = ISL_value()
+  val zero = isl_value.zero
+  println("val is zero? " + isl_value.isZero(zero))
 
-
-  isl.releaseContext(ctx)
+  val neg_one = isl_value.negOne
+  val nan = isl_value.infty
 }
-
